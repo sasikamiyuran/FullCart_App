bf439e8ddeb4b3962807f90c8b8598f312638b1f
sasikamiyuran <sasikamiyuran@gmail.com>
2024-01-21 11:08:11 +0530

    git ignore migration folder

--- FullCart.API/Program.cs
+++ FullCart.API/Program.cs
@@ -1,6 +1,8 @@
 using FullCart.DAL.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add DB Connection
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("ApplicationDbConnection")
     ));
@@ -24,6 +27,29 @@ options =>
 })
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddRoles<IdentityRole>();
+
+// Add Authentication Authorization
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Authentication:Issuer"],
+        ValidAudience = builder.Configuration["Authentication:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeCustomer", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("Role", "Customer");
+    });
+});
 
 var app = builder.Build();
 
